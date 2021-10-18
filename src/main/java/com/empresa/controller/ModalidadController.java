9d5141b7938512836b320e27ef80a3a30b53aa69
MartinECSM <i201811121@cibertec.edu.pe>
2021-10-17 19:23:06 -0500

    Puntos para PC2

--- src/main/java/com/empresa/controller/ModalidadController.java
+++ src/main/java/com/empresa/controller/ModalidadController.java
@@ -6,14 +6,18 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.empresa.entity.FiltroModalidad;
 import com.empresa.entity.Modalidad;
 import com.empresa.service.ModalidadService;
 import com.empresa.util.Constantes;
@@ -48,6 +52,68 @@ public class ModalidadController {
 			e.printStackTrace();
 			salida.put("mensaje", Constantes.MENSAJE_REG_ERROR);
 		}
+		return ResponseEntity.ok(salida);
+	}
+	@GetMapping("/porNombreSede/{paramNombre}/{paramSede}")
+	@ResponseBody
+	public ResponseEntity<Map<String, Object>> listaModalidadPorNombreSede(
+										@PathVariable("paramNombre")String nombre,
+										@PathVariable("paramSede")String sede) {
+		Map<String, Object> salida = new HashMap<String, Object>();
+		try {
+			List<Modalidad> lista = modalidadService .listaModalidadPorNombreSede(nombre, sede);
+			if(CollectionUtils.isEmpty(lista)){
+				salida.put("mensaje", "No existe elementos para la consulta");
+			}else {
+				salida.put("lista", lista);
+				salida.put("mensaje", "Se tiene " + lista.size() + " elementos");
+			}
+		} catch (Exception e) {
+			salida.put("mensaje", "Error : " + e.getMessage());
+		}
+		
+		return ResponseEntity.ok(salida);
+	}
+	@GetMapping("/porNombreSedeConParametros")
+	@ResponseBody
+	public ResponseEntity<Map<String, Object>> listaPorNombreSedeConParametros(
+			@RequestParam(value = "nombre", required = false, defaultValue = "") String nombre,
+			@RequestParam(value = "sede", required = false, defaultValue = "") String sede ){
+		
+		Map<String, Object> salida = new HashMap<String, Object>();
+		try {
+			List<Modalidad> lista = modalidadService.listaModalidadPorNombreSede("%"+nombre+"%" , "%"+sede+"%");
+			if(CollectionUtils.isEmpty(lista)){
+				salida.put("mensaje", "No existe elementos para la consulta");
+			}else {
+				salida.put("lista", lista);
+				salida.put("mensaje", "Se tiene " + lista.size() + " elementos");
+			}
+		} catch (Exception e) {
+			salida.put("mensaje", "Error : " + e.getMessage());
+		}
+		
+		return ResponseEntity.ok(salida);
+	}
+	@GetMapping("/porNombreSedeConJson")
+	@ResponseBody
+	public ResponseEntity<Map<String, Object>>listaPorNombreSedeConParametros (
+							@RequestBody FiltroModalidad filtro) {
+		
+		Map<String, Object> salida = new HashMap<String, Object>();
+		try {
+			filtro.setNombre("%"+filtro.getNombre()+"%");
+			List<Modalidad> lista = modalidadService.listaPorFiltro(filtro);
+			if(CollectionUtils.isEmpty(lista)){
+				salida.put("mensaje", "No existe elementos para la consulta");
+			}else {
+				salida.put("lista", lista);
+				salida.put("mensaje", "Se tiene " + lista.size() + " elementos");
+			}
+		} catch (Exception e) {
+			salida.put("mensaje", "Error : " + e.getMessage());
+		}
+		
 		return ResponseEntity.ok(salida);
 	}
 
--- src/main/java/com/empresa/repository/ModalidadRepository.java
+++ src/main/java/com/empresa/repository/ModalidadRepository.java
@@ -1,10 +1,25 @@
 package com.empresa.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.empresa.entity.FiltroModalidad;
 import com.empresa.entity.Modalidad;
 
 public interface ModalidadRepository extends JpaRepository<Modalidad, Integer>  {
 
+	@Query("select m from Modalidad m where "
+			+ "( :p_nom is '' or m.nombre like :p_nom ) and "
+			+ "( :p_sede is '' or m.sede like :p_sede )")
+	public abstract List<Modalidad> listaModalidadPorNombreSede(
+							 	@Param("p_nom") String nombre, 
+							 	@Param("p_sede") String sede);
 	
+	@Query("select m from Modalidad m where "
+			+ "( :#{#fil.nombre} is '' or m.nombre like :#{#fil.nombre} ) and "
+			+ "( :#{#fil.sede} is ''  or m.sede like :#{#fil.sede} ) ")
+	public abstract List<Modalidad> listaPorFiltro(@Param("fil")FiltroModalidad filtro);
 }
